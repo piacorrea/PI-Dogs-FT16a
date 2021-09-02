fab14797ee8bb432cbe033098ff68d632c576408
piacorrea <pia.correac@usach.cl>
2021-09-02 08:12:59 -0300

    Backend Completed

--- api/src/routes/Temperament.js
+++ api/src/routes/Temperament.js
@@ -8,11 +8,14 @@ const router = require('express').Router();
 router.get('/temperaments', async (req, res) => {
   try {
     let temperamentsApiInfo = (await axios.get(`${URL_APIKEY}`)).data.map(el => el.temperament).join().split(",") // una vez accediendo a los temperamentos, Join los une a todos, luego split los mete en un arreglo separandolos en strings separados por "coma" 
-    let temperamentsEach = temperamentsApiInfo.map((el) => el.trim()) //Trim elimina los especios en blanco en ambos extremos del string
+    let temperamentsEach = temperamentsApiInfo.map((el) => el.trim()).sort() //Trim elimina los especios en blanco en ambos extremos del string y sort los ordena alfabéticamente.
     let noEmptyTemperaments = [] //Temperamentos sin espacios vacíos, pero con temperamentosrepetidos
+    let noRepetedTemperaments = [] 
+    
     temperamentsEach.forEach( el => { if(el !== '') return noEmptyTemperaments.push(el)}) //esto elimina los temperamentos vacíos
+    noEmptyTemperaments.forEach(el => {if(!noRepetedTemperaments.includes(el)) return noRepetedTemperaments.push(el)}) 
 
-    await noEmptyTemperaments.forEach(el => {
+    await noRepetedTemperaments.forEach(el => {
       Temperament.findOrCreate({   //Si está no lo hace nada y si no está lo crea esto me elimina 
        where: {name: el}
       })
@@ -29,5 +32,7 @@ module.exports = router;
 
 /* Para eliminar repetidos sin métodos sequelize    
 
-let noRepetedTemperaments = [] 
-rawTemperaments.forEach(el => {if(!noRepetedTemperaments.includes(el)) return noRepetedTemperaments.push(el)}) */
+ */
+/* .flat() concatena todos los [] en uno solo  */
+/* Para que La BD traiga los datos ordenados
+const allTemperaments = await Temperament.findAll( { order:[ [ 'name','ASC' ] ] } ); */

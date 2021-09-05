ef34f15896698dc72861b3feac0e71f9ede68c81
piacorrea <pia.correac@usach.cl>
2021-09-05 12:02:14 -0300

    Backend 100%

--- api/src/routes/Controllers.js
+++ api/src/routes/Controllers.js
@@ -42,7 +42,7 @@ const getDbInfo = async() => {
  }
 }
 
-const getAllBreeds = async () => {
+const getAllBreeds = async function () {
  try {
     const apiInfo = await getApiInfo();
     const dbInfo = await getDbInfo();
@@ -53,48 +53,5 @@ const getAllBreeds = async () => {
  }
 }
 
-
-
 module.exports = getAllBreeds;
 
-
-/* const getApiInfo = () => {
-
-    let apiP1Promise = axios.get(`${GAMES_APIKEY}`)
-    let apiP2Promise = axios.get(`${GAMES_APIKEY}&page=2`)
-    let apiP3Promise = axios.get(`${GAMES_APIKEY}&page=3`)
-    let apiP4Promise = axios.get(`${GAMES_APIKEY}&page=4`)
-    let apiP5Promise = axios.get(`${GAMES_APIKEY}&page=5`)
-  
-    return Promise.all([apiP1Promise, apiP2Promise, apiP3Promise, apiP4Promise, apiP5Promise])
-    .then(resultados => {
-     var apiP1 = VideogameMap(resultados[0].data.results)
-     var apiP2 = VideogameMap(resultados[1].data.results)
-     var apiP3 = VideogameMap(resultados[2].data.results)
-     var apiP4 = VideogameMap(resultados[3].data.results)
-     var apiP5 = VideogameMap(resultados[4].data.results)
-    
-     return arrayVideogames = apiP1.concat(apiP2, apiP3, apiP4, apiP5)
-  }) .catch(error => next(error))
-  } */
-
-  /* const getNameBreed = async () => {
-  try {
-    let {name} = req.query
-    if(name) {
-    let apiName = (await axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${name}&&api_key=948c432e-503d-4736-8694-a257a7b25bc8`)).data.map(el => {
-        return {
-            id: el.id,
-            name: el.name,
-            image: el.image,
-            height: el.height,
-            weight: el.weight,
-            life_span: el.life_span,
-        };
-      });
-      return apiName
-    } 
-  } catch (error) {
-    console.log(error)
-  }
-} */
